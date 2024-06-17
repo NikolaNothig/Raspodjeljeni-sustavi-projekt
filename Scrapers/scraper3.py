94960e7c46d046566c256f6dd8359fe3e09dc9d5
Nikola <nothignikola@email.com>
2024-06-18 01:03:45 +0200

    Svijet Medija scraper unlimited

--- Scrapers/scraper3.py
+++ Scrapers/scraper3.py
@@ -7,12 +7,21 @@ app = FastAPI()
 def scrape_page(url):
     title = "Svijet Medija"
     response = requests.get(url)
+    
+    if response.status_code != 200:
+        print(f"Failed to retrieve page {url}")
+        return []
+
     soup = BeautifulSoup(response.content, "html.parser")
     
     products = []
     product_titles = soup.find_all("span", class_="field field--name-title field--type-string field--label-hidden")
     actual_prices = soup.find_all("span", class_="product__pricing-discount")
     old_prices = soup.find_all("div", class_="product--teaser__content-price--old")
+    
+    if not product_titles or not actual_prices:
+        print(f"No products found on page {url}")
+        return []
     
     for title, actual_price, old_price in zip(product_titles, actual_prices, old_prices):
         product_name = title.get_text(strip=True)
@@ -33,19 +42,20 @@ async def scrape_all_pages():
     base_url = "https://www.svijet-medija.hr/akcije?items_per_page=24&sort_bef_combine=created_DESC&sort_by=created&sort_order=DESC&page=%2C%2C{page}"
     page_number = 0
     all_products = []
-    max_pages = 10  # Ograničenje na prvih 10 stranica
     
-    while page_number < max_pages:
+    while True:
         url = base_url.format(page=page_number)
         products = scrape_page(url)
         
         if not products:
+            print(f"No products found on page {page_number}. Stopping scraper.")
             break
         
         all_products.extend(products)
         page_number += 1
         print(f"Scraped page number: {page_number}")
     
+    print(f"Total products scraped: {len(all_products)}")
     return {"data": all_products, "title": "Svijet Medija"}
 
 # Pokretanje: uvicorn scraper3:app --reload --port 8003
